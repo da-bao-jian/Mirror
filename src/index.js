a2c1468362de1de0c66e7d8013b8ec3a9bebfbe8
Harry Gao <gaohaoyuandd@gmail.com>
2020-12-07 16:32:46 -0500

    p5 working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import "./styles/index.scss"
 import * as tf from '@tensorflow/tfjs'
+import { fill } from "@tensorflow/tfjs";
+import * as p5 from 'p5'
 
 // function setUp(){
-    tf.tensor([1,2,3,4]).print()
-    console.log('a')
+    // tf.tensor([1,2,3,4]).print()
+    // console.log('a')
 // }
+
+//domcontentloaded might be needed later
+
+const s = (sketch) => {
+    const x=100;
+    const y=100;
+    sketch.setup = function(){
+        sketch.createCanvas(700, 410);
+        sketch.background(0)
+        sketch.ellipse(50,50,80,70)
+    };
+    sketch.draw = function(){
+        sketch.rect(x,y,50,50)
+    }
+    
+
+}
+
+const myCanvas = new p5(s)
+
 

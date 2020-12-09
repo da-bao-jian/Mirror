faec371b8fba16bbeaee3567c9519b73a67d93ea
Harry Gao <gaohaoyuandd@gmail.com>
2020-12-09 10:57:46 -0500

    camera aligned

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import "./styles/index.scss";
 import * as facemeth from '@tensorflow-models/facemesh';
+import TRIANGULATION from './scripts/triangulation';
+import 'regenerator-runtime/runtime';
+//setup webcam 
+async function getCameraReady() {
+    navigator.mediaDevices.getUserMedia({
+        video: {
+            // width: {ideal: 680}, //need to be further adjusted to match canvas
+            // height: {min: 550, ideal: 400, max: 1000},
+            frameRate: {ideal: 12, max:15} 
+        }
+    }).then((stream)=>{
+        document.getElementById('cam').srcObject = stream;
+    }).catch((err)=>{
+        console.log(err);
+    });
+}
 
-//setup webcam and canvas
-navigator.mediaDevices.getUserMedia({
-    video: {
-        width: {min: 1024, ideal: 680, max: 1920},
-        height: {min: 550, ideal: 400, max: 1000},
-        frameRate: {ideal: 12, max:15}
-    }
-}).then((stream)=>{
-    document.getElementById('cam').srcObject = stream;
-});
 
+
+async function main(){
+    await getCameraReady();
+
+}
+// main()
 
 

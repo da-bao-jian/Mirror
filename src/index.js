1ed6d56c3f0125c5b75679e19773733934bb7a3b
Harry Gao <gaohaoyuandd@gmail.com>
2020-12-13 23:05:00 -0500

    drawing triangular completed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles/index.scss";
 import * as facemesh from '@tensorflow-models/face-landmarks-detection';
 import '@tensorflow/tfjs-backend-webgl';
 import TRIANGULATION from './scripts/triangulation';
+import {drawCanvas, drawTriangular} from './scripts/mask_drawing';
 import 'regenerator-runtime/runtime';
 
 let model; 
@@ -20,7 +21,6 @@ function getCameraReady() {
         console.log(err)});
 
     //promise constructor to make sure the webcam reaches certain stage before loading model
-    //more info on promises: https://javascript.info/promise-basics
     const promise = new Promise((resolve, reject)=>{
         cam.onprogress = () => {
             resolve(cam)
@@ -46,33 +46,24 @@ async function runDetection(){
     canvas.height = webCamHeight;
     const ctx = canvas.getContext('2d');
     if(cam.readyState === 4){
-        const drawMask = async() => {
+        const drawMask = async() => { //drawing the mask per frame
             const predictions_arr = await model.estimateFaces({input: webCam}); //scaledMesh.length = 478 because of using the updated Iris model; original scaledMesh should only have 468 end points
             ctx.drawImage(webCam, 0, 0, webCam.width, webCam.height, 0,0, canvas.width, canvas.height);
             if(predictions_arr.length !== 0){
                 console.log(predictions_arr) 
                 for(let i=0; i<predictions_arr.length;i++){ //could this just be predictions_arr[0]?
                     const keypoints = predictions_arr[i].scaledMesh; //getting the nomalized 3d coordination landmarks (scaledMesh is the normalized coordination)
-                    for(let j = 0; j<keypoints.length; j++){
-                        const x = keypoints[j][0]; //x and y are horizontal and vertical coordination; z is the depth coordination derived from x using weak perspective projection
-                        const y = keypoints[j][1];
-                        ctx.beginPath();
-                        ctx.arc(x,y,1, 0, 2*Math.PI);
-                        ctx.fillStyle = 'aqua';
-                        ctx.fill();
-                    }
+                    // drawCanvas(keypoints, ctx);
+                    drawTriangular(keypoints, ctx);
                 }
             }
+            //recursively calling itself to continously run the detection per frame
             requestAnimationFrame(drawMask);
         }
         drawMask()
     };
-    //recursively calling itself to continously run the detection per frame
-    
     
 }
-
-
 
 
 async function main(){
@@ -80,9 +71,6 @@ async function main(){
     model = await facemesh.load(
         facemesh.SupportedPackages.mediapipeFacemesh);
     // await getCameraReady();
-    // setInterval(()=>{
-    //     runDetection();
-    // }, 10)
     runDetection();
     
 }
--- src/scripts/mask_drawing.js
+++ src/scripts/mask_drawing.js
@@ -1,2 +1,33 @@
-//using scalemesh from detection data to 
+import {TRIANGULATION} from './triangulation';
+
+
+export function drawCanvas(keypoints, ctx){
+    for(let j = 0; j<keypoints.length; j++){
+        const x = keypoints[j][0]; //x and y are horizontal and vertical coordination; z is the depth coordination derived from x using weak perspective projection
+        const y = keypoints[j][1];
+        ctx.beginPath();
+        ctx.arc(x,y,1, 0, 2*Math.PI);
+        ctx.fillStyle = 'aqua';
+        ctx.fill();
+    }
+}; 
+
+export function drawTriangular(keypoints, ctx){ 
+    for (let i = 0; i < TRIANGULATION.length; i+=3) {
+        const points = [
+        TRIANGULATION[i], TRIANGULATION[i + 1],
+        TRIANGULATION[i + 2]
+        ].map(index => keypoints[index]);
+
+        const region = new Path2D();
+        region.moveTo(points[0][0], points[0][1]);
+        for(let j = 1; j<points.length;j++){
+            region.lineTo(points[j][0], points[j][1]);
+        }
+        region.closePath();
+        ctx.strokeStyle='aqua';
+        ctx.stroke(region);
+    }   
+};    
+
 

2578a9701d87c3685903b961aef21040b6ba8c4f
Harry Gao <gaohaoyuandd@gmail.com>
2020-12-15 01:02:08 -0500

    dual display complete

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./styles/index.scss";
 import * as facemesh from '@tensorflow-models/face-landmarks-detection';
 import '@tensorflow/tfjs-backend-webgl';
 import TRIANGULATION from './scripts/triangulation';
-import {drawCanvas, drawTriangular, drawTriangularHue} from './scripts/mask_drawing';
+import {drawCanvas, drawTriangular, drawTriangularHue, drawTriangularBWG} from './scripts/mask_drawing';
 import 'regenerator-runtime/runtime';
 
 let model; 
@@ -13,7 +13,7 @@ function getCameraReady() {
     const cam = document.getElementById('cam');
     navigator.mediaDevices.getUserMedia({
         video: {
-            frameRate: {ideal: 12, max:15} 
+            frameRate: {ideal: 10, max:15} 
         }
     }).then((stream)=>{
         cam.srcObject = stream;
@@ -38,24 +38,39 @@ function getCameraReady() {
 
 //run detection
 async function runDetection(){
+
     const webCam = document.getElementById('cam');
     const webCamWidth = webCam.width;
     const webCamHeight = webCam.height;
+
     const canvas = document.getElementById('canvas');
     canvas.width = webCamWidth;
     canvas.height = webCamHeight;
-    const ctx = canvas.getContext('2d');
+    const ctx1 = canvas.getContext('2d');
+
+    const second_canvas = document.getElementById('second-canvas');
+    const second_canvasWidth = second_canvas.width;
+    const second_canvasHeight = second_canvas.height;
+
+    const face = document.getElementById('second-canvas');
+    face.width = second_canvasWidth;
+    face.height = second_canvasHeight;
+    const ctx2 = face.getContext('2d');
+
     if(cam.readyState === 4){
         const drawMask = async() => { //drawing the mask per frame
             const predictions_arr = await model.estimateFaces({input: webCam}); //scaledMesh.length = 478 because of using the updated Iris model; original scaledMesh should only have 468 end points
-            ctx.drawImage(webCam, 0, 0, webCam.width, webCam.height, 0,0, canvas.width, canvas.height);
+            ctx1.drawImage(webCam, 0, 0, webCam.width, webCam.height, 0,0, canvas.width, canvas.height);
+            ctx2.drawImage(second_canvas, 0, 0, webCam.width, webCam.height, 0,0, face.width, face.height);
             if(predictions_arr.length !== 0){
                 console.log(predictions_arr) 
                 for(let i=0; i<predictions_arr.length;i++){ //could this just be predictions_arr[0]?
                     const keypoints = predictions_arr[i].scaledMesh; //getting the nomalized 3d coordination landmarks (scaledMesh is the normalized coordination)
-                    // drawCanvas(keypoints, ctx);
-                    // drawTriangular(keypoints, ctx);
-                    drawTriangularHue(keypoints, ctx)
+                    drawCanvas(keypoints, ctx1);
+
+                    drawTriangular(keypoints, ctx2, face);
+                    
+                    // drawTriangularHue(keypoints, ctx2, face)
                 }
             }
             //recursively calling itself to continously run the detection per frame
--- src/scripts/mask_drawing.js
+++ src/scripts/mask_drawing.js
@@ -1,7 +1,10 @@
+//consider using dat-gui for color control
+
 import {TRIANGULATION} from './triangulation';
 
 
-export function drawCanvas(keypoints, ctx){
+export function drawCanvas(keypoints, ctx, face=null){
+    face === null ? null : ctx.clearRect(0, 0, face.width, face.height);
     for(let j = 0; j<keypoints.length; j++){
         const x = keypoints[j][0]; //x and y are horizontal and vertical coordination; z is the depth coordination derived from x using weak perspective projection
         const y = keypoints[j][1];
@@ -12,7 +15,8 @@ export function drawCanvas(keypoints, ctx){
     }
 }; 
 
-export function drawTriangular(keypoints, ctx){ 
+export function drawTriangular(keypoints, ctx, face=null){ 
+    face === null ? null : ctx.clearRect(0, 0, face.width, face.height);
     for (let i = 0; i < TRIANGULATION.length; i+=3) {
         const points = [
         TRIANGULATION[i], TRIANGULATION[i + 1],
@@ -28,11 +32,11 @@ export function drawTriangular(keypoints, ctx){
         ctx.strokeStyle='aqua';
         ctx.stroke(region);
 
-
     }   
 };    
 
-export function drawTriangularHue(keypoints, ctx){ 
+export function drawTriangularHue(keypoints, ctx, face=null){ 
+    face === null ? null : ctx.clearRect(0, 0, face.width, face.height);
     for (let i = 0; i < TRIANGULATION.length; i+=3) {
         const points = [
         TRIANGULATION[i], TRIANGULATION[i + 1],
@@ -41,18 +45,40 @@ export function drawTriangularHue(keypoints, ctx){
 
         //random color generation
         const spectrum = '1234567890ABCDEF';
-        const color = '#';
+        const hue = '#';
         for(let i = 0; i < 6; i++){
-            color += spectrum[Math.floor(Math.random()*16)];
+            hue += spectrum[Math.floor(Math.random()*16)];
         };
         
         ctx.beginPath()
         ctx.moveTo(points[0][0], points[0][1]);
         ctx.lineTo(points[1][0], points[1][1]);
         ctx.lineTo(points[2][0], points[2][1]);
-        ctx.fillStyle=color;
+        ctx.fillStyle = hue;
         ctx.fill();
     }   
 };  
+
+export function drawTriangularBWG(keypoints, ctx){ 
+    for (let i = 0; i < TRIANGULATION.length; i+=3) {
+        const points = [
+        TRIANGULATION[i], TRIANGULATION[i + 1],
+        TRIANGULATION[i + 2]
+        ].map(index => keypoints[index]);
+
+        //bwg color generation
+        const spectrum = ['#f5f5f5', '#808080', '#000000'];
+        const bwg = spectrum[Math.floor(Math.random()*3)];
+        
+        ctx.beginPath()
+        ctx.moveTo(points[0][0], points[0][1]);
+        ctx.lineTo(points[1][0], points[1][1]);
+        ctx.lineTo(points[2][0], points[2][1]);
+        ctx.fillStyle = bwg;
+        ctx.fill();
+    }   
+};  
+
+
 
 
